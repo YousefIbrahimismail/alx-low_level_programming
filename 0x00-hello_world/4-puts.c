0cf3c4db4d0cd2d4d5fc79c9c3e988cc609ee5a0
yousefibrahimismail <yousef_i44@protonmail.com>
2022-10-16 22:35:49 +0200

    Working with C day-04

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
+/* 
+  * main the main function of the program 
+ */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
