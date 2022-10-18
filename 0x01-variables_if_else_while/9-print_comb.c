a5801aaae1169d61cd73c7d9933b946d17ccf160
yousefibrahimismail <yousef_i44@protonmail.com>
2022-10-18 02:20:23 +0200

    Working with C day-06

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Prints all possible combinations of single digit numbers
+ * Return: Should return zero
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 48; a < 58; a++)
+	{
+		putchar(a);
+		if (a != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
